20cb52288c4251aca056ff1788ba3fff87faca1f
= <=>
2021-10-11 23:39:16 +0900

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Footer.module.css';
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+      <p>運営者について　プライバシー・ポリシー　お問い合わせ</p>
+      <br />
+      <p>© 2021 Goodsist</p>
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/dist/client/link';
+import styles from '../styles/Header.module.css';
+
+export default function Header() {
+  return (
+    <div className={styles.header_container}>
+      <div className={styles.header}>
+        <div className={styles.logo_container}>
+          <Link href='/'>
+            <a>
+              <p className={styles.logo}>Goodsist</p>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
