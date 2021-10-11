eb77ee1092354803b4132574153734d38fe99d94
= <=>
2021-10-12 00:28:48 +0900

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,7 @@ export async function getStaticProps() {
 // }
 
 // const Home: NextPage = ({ eventList }) => {
-const Home: NextPage = ({ eventList }: PageProps) => {
+const Home: NextPage = (eventList) => {
   return (
     <>
       <Head>
