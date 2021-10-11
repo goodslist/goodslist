8abf7f7d5d1945307505f2fb608289a772bba1d8
= <=>
2021-10-12 02:43:04 +0900

    first commit

--- src/components/supabase.tsx
+++ src/components/supabase.tsx
@@ -1,15 +1,17 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
 // 環境変数のロード
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY
 
-// 環境変数が設定されていなかったらエラーを投げる
+// URLの環境変数が設定されていなかったらエラー
 if (!SUPABASE_URL) {
-  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL');
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
 }
+// KEYの環境変数が設定されていなかったらエラー
+
 if (!SUPABASE_KEY) {
-  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_KEY');
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_KEY')
 }
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
