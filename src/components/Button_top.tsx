4f826fb9939016df6936b97e2f1d44e1554c95f9
= <=>
2021-10-15 09:01:25 +0900

    first commit

--- src/components/Button_top.tsx
+++ src/components/Button_top.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 
 export default function Button_top() {
   // scroll が高さを超えているとき trueになるstate
-  const [scroll, setScroll] = React.useState(styles.button_scroll)
+  const [scroll, setScroll] = React.useState(styles.button_scroll_active)
   const returnButtonTop = () => {
     window.scrollTo({
       top: 0,
@@ -14,12 +14,12 @@ export default function Button_top() {
 
   useEffect(() => {
     const scrollAction = () => {
-      if (150 > window.scrollY) {
-        // 150の値は 判定したい高さに変更する
-        setScroll(styles.button_scroll)
-      } else {
-        setScroll(styles.button_scroll_active)
-      }
+      // if (150 > window.scrollY) {
+      //   // 150の値は 判定したい高さに変更する
+      //   setScroll(styles.button_scroll)
+      // } else {
+      //   setScroll(styles.button_scroll_active)
+      // }
     }
     window.addEventListener('scroll', scrollAction, {
       capture: false,
