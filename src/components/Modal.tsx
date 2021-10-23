85a77c3fad9967a7023e5b5ec39dd0ac765fd26a
= <=>
2021-10-24 05:39:34 +0900

    first commit

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,24 +1,59 @@
 import React from 'react'
+import styles from '../styles/Modal.module.css'
+import { useState } from 'react'
 
-const Modal = (props: any) => {
+const useModal = (props: any) => {
   const closeModal = () => {
     props.setShowModal(false)
   }
 
   return (
     <>
-      {props.showFlag ? ( // showFlagがtrueだったらModalを表示する
-        <div id='overlay'>
-          <div id='modalContent'>
-            <p>This is ModalContent</p>
-            <button onClick={closeModal}>Close</button>
-          </div>
+      <div
+        id='overlay'
+        className={props.showFlag ? styles.overlay_active : styles.overlay}
+        onClick={closeModal}
+      >
+        <div id='modalContent' className={styles.modal_container}>
+          <p>This is ModalContent</p>
+          <button onClick={closeModal}>Close</button>
         </div>
-      ) : (
-        <></> // showFlagがfalseの場合はModalは表示しない
-      )}
+      </div>
     </>
   )
 }
 
-export default Modal
+export default useModal
+
+// import React from 'react'
+// import styles from '../styles/Modal.module.css'
+
+// const Modal = (props: any) => {
+//   const closeModal = () => {
+//     props.setShowModal(false)
+//   }
+
+//   return (
+//     <>
+//       {props.showFlag ? ( // showFlagがtrueだったらModalを表示する
+//         <div id='overlay' className={styles.overlay_active}>
+//           <div id='modalContent'>
+//             <p>This is ModalContent</p>
+//             <button onClick={closeModal}>Close</button>
+//           </div>
+//         </div>
+//       ) : (
+//         <>
+//           <div id='overlay' className={styles.overlay}>
+//             <div id='modalContent'>
+//               <p>This is ModalContent</p>
+//               <button onClick={closeModal}>Close</button>
+//             </div>
+//           </div>
+//         </> // showFlagがfalseの場合はModalは表示しない
+//       )}
+//     </>
+//   )
+// }
+
+// export default Modal
