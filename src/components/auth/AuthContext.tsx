d6b774e9d204b71153280688491d8449be78adef
= <=>
2021-10-29 21:34:21 +0900

    first commit

--- src/components/auth/AuthContext.tsx
+++ src/components/auth/AuthContext.tsx
@@ -75,6 +75,8 @@ export const AuthProvider: FC = ({ children }) => {
         user,
         session,
         signOut,
+        avatar_url: 'https://',
+        sign_up: false,
       }}
     >
       {children}
