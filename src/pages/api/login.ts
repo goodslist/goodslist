0d750b79f4b2c363f2f06441493eb229391c1e65
= <=>
2021-11-14 05:56:17 +0900

    first commit

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,6 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { supabase } from '../../components/supabase'
 import cookie from 'cookie'
 
 export default async function login(req: NextApiRequest, res: NextApiResponse) {
-  res.json({ message: 'something wrong !' })
+  //POST以外は404を返す
+  if (req.method !== 'POST') return res.status(404).send('Not Found')
+
+  const validSignedUp = async () => {
+    // const email = 'tnvzk679@yahoo.co.jp'
+    const { data, error } = await supabase.from('events').select('event_name').eq('event_id', 2)
+
+    res.json({ message: 'something wronga !', data: data })
+  }
+  validSignedUp()
 }
