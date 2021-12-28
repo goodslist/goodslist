6b9a23cd9b034d333c3fbfe97adab6d1f139aa68
goodslist <goodslist2021@gmail.com>
2021-12-28 17:40:50 +0900

    first commit

--- src/components/auth/AuthContext.tsx
+++ src/components/auth/AuthContext.tsx
@@ -8,26 +8,10 @@ import React, {
   useLayoutEffect,
 } from 'react'
 import { useRouter } from 'next/router'
-import { initializeApp } from 'firebase/app'
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
 // import { User } from 'firebase/auth'
 import { supabase } from '../supabase'
 
 import { User } from '../types'
-
-//firebaseを初期化
-export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-}
-const firebaseApp = initializeApp(firebaseConfig)
-
-const auth = getAuth()
 
 interface AuthContextProps {
   currentUser: User | null | undefined
