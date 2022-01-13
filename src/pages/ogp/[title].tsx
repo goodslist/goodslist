5dfa2e426eb4e8f2516257ab985eb1f5862b2633
goodslist <goodslist2021@gmail.com>
2022-01-14 02:56:38 +0900

    first commit

--- src/pages/ogp/[title].tsx
+++ src/pages/ogp/[title].tsx
@@ -73,7 +73,7 @@ export const getServerSideProps: GetServerSideProps = async (
           justify-content: center;
           align-items: center;
           width: 80%;
-          height: 167px;
+          height: 169px;
           margin: 0 auto;
           color: #374151;
           font-size: 4rem;
@@ -88,7 +88,7 @@ export const getServerSideProps: GetServerSideProps = async (
           justify-content: center;
           align-items: start;
           width: 80%;
-          height: 296px;
+          height: 292px;
           margin: 0 auto;
           text-align:left;
           color: #374151;
@@ -104,7 +104,7 @@ export const getServerSideProps: GetServerSideProps = async (
           justify-content: center;
           align-items: center;
           width: 80%;
-          height: 167px;
+          height: 169px;
           margin: 0 auto;
           color: #374151;
           font-size: 4rem;
