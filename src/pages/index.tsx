39eefd846f5f7d8a5fa440d286bf6b1c90084bc9
goodslist <goodslist2021@gmail.com>
2022-01-15 02:04:12 +0900

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -152,7 +152,8 @@ const Home = ({ eventList }: Props) => {
       <div className={styles.wrapper_white}>
         <main className={styles.main}>
           <h1 className={styles.title}>
-            GOODS<br></br>LIST
+            GOODSist
+            {/* GOODS<br></br>LIST */}
           </h1>
           <h2 className={styles.sub_title}>グッズ代が計算できるWEBアプリ</h2>
           <form className={styles.search_container} onSubmit={enterForm}>
