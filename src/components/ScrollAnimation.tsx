30004e3d5b47c9acd1718e35cb90c2e547467df8
goodslist <goodslist2021@gmail.com>
2022-01-15 18:37:46 +0900

    first commit

---
+++ src/components/ScrollAnimation.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+
+type Props = {
+  index: number
+  onIntersection?: (index: number) => void
+}
+
+const Container: React.FC<Props> = ({ index, onIntersection, children }) => {
+  const ref = React.useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          if (onIntersection !== undefined) {
+            onIntersection(index)
+          }
+        }
+      },
+      { threshold: 0.5 },
+    )
+
+    if (ref.current === null) return
+
+    observer.observe(ref.current)
+
+    const { current } = ref
+
+    return () => {
+      observer.unobserve(current)
+    }
+  }, [])
+
+  return <div ref={ref}>{children}</div>
+}
+
+export default Container
