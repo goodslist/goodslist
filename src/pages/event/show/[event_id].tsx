61637e495bf7b6a9089ffe243bbba2e753af97c0
goodslist <goodslist2021@gmail.com>
2022-01-21 17:36:31 +0900

    first commit

--- src/pages/event/show/[event_id].tsx
+++ src/pages/event/show/[event_id].tsx
@@ -498,7 +498,7 @@ const ShareList = ({ propsEvent, propsShowItems, propsShowGroups }: Props) => {
             <SocialButton provider='LINE' />
           </a>
           <a
-            href='mailto:info@example.com?subject=問sい合わせ&amp;body=ご記入ください'
+            href={'mailto:info@example.com?subject=問sい合わせ&amp;body=ご記入ください' + 'aaa'}
             target='_blank'
           >
             <SocialButton provider='メール' />
