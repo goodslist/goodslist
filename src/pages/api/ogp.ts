e628a9e9b2439a38918b0b79dabbb77ef6d98797
goodslist <goodslist2021@gmail.com>
2022-01-23 08:18:02 +0900

    first commit

--- src/pages/api/ogp.ts
+++ src/pages/api/ogp.ts
@@ -5,31 +5,22 @@ const createOgp = async (req: NextApiRequest, res: NextApiResponse): Promise<voi
   const { id } = req.query
   const WIDTH = 1200 as const
   const HEIGHT = 630 as const
+  const DX = 0 as const
+  const DY = 0 as const
   const canvas = createCanvas(WIDTH, HEIGHT)
   const ctx = canvas.getContext('2d')
   // registerFont(path.resolve('.fonts/NotoSansJP-Regular.otf'), {
   //   family: 'Noto',
   // })
-  // ctx.fillStyle = '#FFF'
-  // ctx.fillRect(0, 0, WIDTH, HEIGHT)
-  // ctx.font = '60px ipagp'
-  // ctx.fillStyle = '#000000'
-  // ctx.textAlign = 'center'
-  // ctx.textBaseline = 'middle'
-  // const text = '入力した文字は' + String(id) + 'なのねん'
-  // ctx.fillText(text, WIDTH / 2, HEIGHT / 2)
-  // const buffer = canvas.toBuffer()
-  // res.writeHead(200, {
-  //   'Content-Type': 'image/png',
-  //   'Content-Length': buffer.length,
-  // })
-  // res.end(buffer, 'binary')
-  // res.send('aaHTTP POST request sent to the webhook URL!')
-  const DX = 0 as const
-  const DY = 0 as const
 
   ctx.fillStyle = '#FFF'
   ctx.fillRect(DX, DY, WIDTH, HEIGHT)
+  ctx.font = '60px ipagp'
+  ctx.fillStyle = '#000000'
+  ctx.textAlign = 'center'
+  ctx.textBaseline = 'middle'
+  const text = '入力した文字は' + String(id) + 'なのねん'
+  ctx.fillText(text, WIDTH / 2, HEIGHT / 2)
 
   const buffer = canvas.toBuffer()
 
