c5b921c27d6ccb9051f266f3581211b88c286525
goodslist <goodslist2021@gmail.com>
2022-01-23 08:03:25 +0900

    first commit

--- src/pages/api/ogp.ts
+++ src/pages/api/ogp.ts
@@ -7,9 +7,9 @@ const createOgp = async (req: NextApiRequest, res: NextApiResponse): Promise<voi
   const HEIGHT = 630 as const
   const canvas = createCanvas(WIDTH, HEIGHT)
   const ctx = canvas.getContext('2d')
-  // registerFont(path.resolve('.fonts/NotoSansJP-Regular.otf'), {
-  //   family: 'Noto',
-  // })
+  registerFont(path.resolve('.fonts/NotoSansJP-Regular.otf'), {
+    family: 'Noto',
+  })
   // ctx.fillStyle = '#FFF'
   // ctx.fillRect(0, 0, WIDTH, HEIGHT)
   // ctx.font = '60px ipagp'
