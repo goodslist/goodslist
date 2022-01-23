671d0192a04ef9894ab7c00a11af6b4bf2bd7532
goodslist <goodslist2021@gmail.com>
2022-01-23 20:09:26 +0900

    first commit

--- src/pages/api/ogp.ts
+++ src/pages/api/ogp.ts
@@ -16,12 +16,26 @@ const createOgp = async (req: NextApiRequest, res: NextApiResponse): Promise<voi
 
   ctx.fillStyle = '#FFF'
   ctx.fillRect(DX, DY, WIDTH, HEIGHT)
-  ctx.font = '60px ipagp'
+  ctx.font = '40px ipagp'
   ctx.fillStyle = '#000000'
   // ctx.textAlign = 'center'
   // ctx.textBaseline = 'middle'
   const text = String(id)
   ctx.fillText(text, 150, 150)
+  ctx.font = '100px ipagp'
+  const text2 = String('1st TOUR 2021 ')
+  ctx.fillText(text2, 300, 300)
+  // ctx.fillText(text2, 150, 250)
+  // const text3 = String('COLORFUL LIVE 1st - Link -')
+  // ctx.fillText(text3, 150, 330)
+  // const text4 = String('Love the Life We Live～')
+  // ctx.fillText(text4, 150, 410)
+  ctx.font = '30px ipagp'
+  const text5 = String('イベントのグッズ代が計算できるWEBアプリ')
+  ctx.fillText(text5, 150, 520)
+  ctx.font = '60px ipagp'
+  const text6 = String('GOODSist')
+  ctx.fillText(text6, 800, 530)
 
   const buffer = canvas.toBuffer()
 
