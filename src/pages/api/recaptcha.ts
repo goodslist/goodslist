c45a99d2c12a9d0764908b8f7aad731cac15e14b
goodslist <goodslist2021@gmail.com>
2022-01-25 08:22:12 +0900

    first commit

--- src/pages/api/recaptcha.ts
+++ src/pages/api/recaptcha.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest as Req, NextApiResponse as Res } from 'next'
 import axios from 'axios'
+import qs from 'qs'
 
 export default async function recaptcha(req: Req, res: Res) {
   // "POST"以外は、"404 Not Found"を返す
@@ -19,6 +20,17 @@ export default async function recaptcha(req: Req, res: Res) {
   //     res.send(JSON.stringify({ status: 'エラー' }))
   //   })
 
+
+  let params = new URLSearchParams();
+  params.append('secret', String(process.env.NEXT_PUBLIC_RECAPTCHA_SECRETKEY));
+  params.append('response', token);
+  await axios.post('https://www.google.com/recaptcha/api/siteverify', params).then((result) => {
+        res.send(JSON.stringify({ status: '成功', data: result.data }))
+      })
+      .catch(() => {
+        res.send(JSON.stringify({ status: 'エラー2' }))
+      })
+
   // await axios({
   //   method: 'post',
   //   headers: {
@@ -37,24 +49,24 @@ export default async function recaptcha(req: Req, res: Res) {
   //     res.send(JSON.stringify({ status: 'エラー2' }))
   //   })
 
-  await fetch('https://www.google.com/recaptcha/api/siteverify', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-      secret: '6LccjjUeAAAAAO1GxRTUqPM9WB9TeYYoWIQJzkNQ', // シークレット
-      response: token, // トークン
-    }),
-  })
-    .then((result) => {
-      res.send(JSON.stringify({ status: '成功', data: result }))
-    })
-    .catch(() => {
-      res.send(JSON.stringify({ status: 'エラーなのだ' }))
-    })
-  res.send(JSON.stringify({ status: '終わり' }))
-}
+//   await fetch('https://www.google.com/recaptcha/api/siteverify', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     body: new URLSearchParams({
+//       secret: '6LccjjUeAAAAAO1GxRTUqPM9WB9TeYYoWIQJzkNQ', // シークレット
+//       response: token, // トークン
+//     }),
+//   })
+//     .then((result) => {
+//       res.send(JSON.stringify({ status: '成功', data: result }))
+//     })
+//     .catch(() => {
+//       res.send(JSON.stringify({ status: 'エラーなのだ' }))
+//     })
+//   res.send(JSON.stringify({ status: '終わり' }))
+// }
 
 // const photo = (JSON.parse(req.body).photo || '').toString()
 // セッションIDをCookieに設定する
