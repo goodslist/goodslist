e225b6f1e45990fb273d6c7fa0b75764d56c272e
goodslist <goodslist2021@gmail.com>
2022-01-27 07:12:27 +0900

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import styles from '../styles/Footer.module.css'
 import Link from 'next/link'
+import Twitter from '../../public/images/twitter.svg'
 
 export default function Footer() {
   const currentTime = new Date()
@@ -8,6 +9,9 @@ export default function Footer() {
     <>
       <div className={styles.footer_container}>
         <p className={styles.footer_twitter}>Follow me!!</p>
+        <span className={styles.footter_twitter_icon}>
+          <Twitter />
+        </span>
         <p className={styles.footer_nav}>
           <Link href={'/'}>
             <a>Home</a>
