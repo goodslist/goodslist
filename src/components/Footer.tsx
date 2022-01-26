f4beb63b57a57c1ba3d3bea4539de7dc13189d68
goodslist <goodslist2021@gmail.com>
2022-01-27 07:32:36 +0900

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,10 +8,17 @@ export default function Footer() {
   return (
     <>
       <div className={styles.footer_container}>
-        <p className={styles.footer_twitter}>Follow me!!</p>
-        <span className={styles.footter_twitter_icon}>
-          <Twitter />
-        </span>
+        <Link href={'https://twitter.com/goodsistjp'}>
+          <a target='_blank'>
+            <p className={styles.footer_twitter}>
+              Follow me!!
+              <span />
+              <span className={styles.footter_twitter_icon}>
+                <Twitter alt='Twitter acount link' />
+              </span>
+            </p>
+          </a>
+        </Link>
         <p className={styles.footer_nav}>
           <Link href={'/'}>
             <a>Home</a>
